0ee487c838d64a52028e3e9e8499632f5e993c49
Alexander Cleoni <alexander.cleoni@gmail.com>
2018-08-09 10:18:46 -0400

    redux implemented

---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Parallax} from 'react-scroll-parallax'
+
+
+export default class Home extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            
+        }
+    }
+}
---
+++ src/redux/store.js
@@ -0,0 +1,23 @@
+import {createStore, applyMiddleware, compose} from 'redux';
+import { persistReducer } from 'redux-persist';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
+const persistConfig = {
+    key: 'root',
+    storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const initState = {};
+const middleware = [thunk];
+
+export const store = createStore(
+    persistedReducer,
+    initState,
+    compose(
+        applyMiddleware(...middleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+);
