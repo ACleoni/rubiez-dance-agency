075f5d66b97b66799e3236cc48307448992fb2de
Alexander Cleoni <alexander.cleoni@gmail.com>
2018-08-09 09:35:40 -0400

    Unit testing added and configured for future use - Test Framework is Jest

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import App from './App';
+import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
